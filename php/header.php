af9dcb9f0179216ef01545c9c069c65d0b396146
RoanvDam <114396994+RoanvdamSchool@users.noreply.github.com>
2024-02-28 11:21:18 +0100

    style.css toegevoegd
    alle styling heeft zijn eigen sectie voor overzichtelijkheid

--- php/header.php
+++ php/header.php
@@ -8,13 +8,15 @@
     </head>
     <header>
         <div id="header_top_layer">
-            <img src="img/logo.png" alt="site_logo">
-            <a>login</a>
-            <a>registreer</a>
-            <i class="fa-solid fa-cart-shopping"></i>
+            <a href="index.php"><img src="img/logo.png" alt="site_logo" class="site_logo"></a>
+            <a href="#">login</a>
+            <a href="#">registreer</a>
+            <a href="cart.php" class="shopping_cart"><i class="fa-solid fa-cart-shopping"></i></a>
         </div>
         <div id="header_bot_layer">
-            
+            <a href="#">offerte</a>
+            <a href="#">dashboard</a>
+            <a href="#">contact</a>
         </div>
     </header>
     <body>
