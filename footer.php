e3b2191d2aa6c4d30be284b46f23560f21784109
RoanvDam <114396994+RoanvdamSchool@users.noreply.github.com>
2024-02-28 10:25:33 +0100

    winkewagen

---
+++ footer.php
@@ -0,0 +1,4 @@
+
+        <script src="https://kit.fontawesome.com/74f00c029b.js" crossorigin="anonymous"></script>
+    </body>
+</html>
---
+++ header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <title>Clearsky Solar</title>
+    </head>
+    <header>
+        <i class="fa-solid fa-cart-shopping"></i>
+    </header>
+    <body>
