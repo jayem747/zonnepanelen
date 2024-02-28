e46ad159c97b252104ab3e997e1a12ce90bf98bb
RoanvDam <114396994+RoanvdamSchool@users.noreply.github.com>
2024-02-28 10:48:11 +0100

    .

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php
-require_once("header.php");
+require_once("php/header.php");
 ?>
+<div id="main_content">
 
+</div>
 <?php
-require_once("footer.php");
+require_once("php/footer.php");
 ?>
---
+++ php/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Clearsky Solar</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <link rel="icon" type="image/x-icon" href="img/favicon.ico">
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+    <header>
+        <div id="header_top_layer">
+            <img src="img/logo.png" alt="site_logo">
+            <a>login</a>
+            <a>registreer</a>
+            <i class="fa-solid fa-cart-shopping"></i>
+        </div>
+        <div id="header_bot_layer">
+            
+        </div>
+    </header>
+    <body>
