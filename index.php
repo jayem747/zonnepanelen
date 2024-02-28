77101a68de7771fde8e3d112f5243580199acfa6
RoanvDam <114396994+RoanvdamSchool@users.noreply.github.com>
2024-02-28 13:38:04 +0100

    product_container bijna af

--- index.php
+++ index.php
@@ -18,19 +18,25 @@ require_once("php/header.php");
     <!-- container of all the products -->
     <section id="products_grid">
         <section class="product_container">
-            <img src="img/solar_image.png" class="product_image">
+            <section class="color_product_image">
+                <img src="img/solar_image.png" class="product_image">
+            </section>
             <h2>basispakket</h2>
             <p>Ontdek ons instappakket: 4 zonnepanelen, omvormer en installatie. Bespaar direct op uw energierekening!</p>
             <a href="product.php?productID=#" class="view_product_button">bekijk product</a>
         </section>
         <section class="product_container">
-            <img src="img/solar_image.png" class="product_image">
+            <section class="color_product_image">
+                <img src="img/solar_image.png" class="product_image">
+            </section>
             <h2>basispakket</h2>
             <p>Ontdek ons instappakket: 4 zonnepanelen, omvormer en installatie. Bespaar direct op uw energierekening!</p>
             <a href="product.php?productID=#" class="view_product_button">bekijk product</a>
         </section>
         <section class="product_container">
-            <img src="img/solar_image.png" class="product_image">
+            <section class="color_product_image">
+                <img src="img/solar_image.png" class="product_image">
+            </section>
             <h2>basispakket</h2>
             <p>Ontdek ons instappakket: 4 zonnepanelen, omvormer en installatie. Bespaar direct op uw energierekening!</p>
             <a href="product.php?productID=#" class="view_product_button">bekijk product</a>
