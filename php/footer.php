13e5f4e6830d61c0b9658fd09e80b54489be3852
RoanvDam <114396994+RoanvdamSchool@users.noreply.github.com>
2024-02-29 09:27:15 +0100

    homepagina af

--- php/footer.php
+++ php/footer.php
@@ -1,4 +1,31 @@
-        <section class="divider_150px"></section>
+        <footer>
+            <section>
+                <p>Samen op weg naar een groenere toekomst. Ontdek onze solarpakketten en maak vandaag nog de overstap naar duurzame energie.</p>
+            </section>
+            <section>
+                <h2>Neem contact met ons op</h2>
+                <table>
+                    <tr><th></th><th></th></tr>
+                    <tr>
+                        <td><i class="fa-solid fa-phone"></td>
+                        <td>0612345678</td>
+                    </tr>
+                    <tr>
+                        <td><i class="fa-solid fa-building"></i></td>
+                        <td>Zonnestraat 20</br>1235 XY, Gouda</td>
+                    </tr>
+                    <tr>
+                        <td><i class="fa-solid fa-at"></td>
+                        <td>helpdesk@clearskysolar.nl</td>
+                    </td>
+                </table>
+                <!--ul>
+                    <li><i class="fa-solid fa-phone"></i>06-12345678</li>
+                    <li><i class="fa-solid fa-building"></i>Zonnestraat 20</br>1235 XY, Gouda</li>
+                    <li><i class="fa-solid fa-at"></i>helpdesk@clearskysolar.nl</li>
+                </ul-->
+            </section>
+        </footer>
         <script src="https://kit.fontawesome.com/74f00c029b.js" crossorigin="anonymous"></script>
     </body>
 </html>
