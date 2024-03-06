0a799b74212a11e84eb43e24a6c4d91f5e396926
Yorrick06 <yorrick06@casema.nl>
2024-03-06 10:47:57 +0100

    Update database_function.php

--- php/database_function.php
+++ php/database_function.php
@@ -9,7 +9,7 @@ function pdoObject($dbname) {
         // set the PDO error mode to exception
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     } catch(PDOException $e) {
-        echo $sql . "<br>" . $e->getMessage();
+        echo $e->getMessage();
     }
     return($conn);
 }
