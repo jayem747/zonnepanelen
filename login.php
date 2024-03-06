c20ce84750c788ee9eb77fb803f0fe03b904ea49
RoanvDam <114396994+RoanvdamSchool@users.noreply.github.com>
2024-03-06 09:00:20 +0100

    login.php toegevoegd

---
+++ login.php
@@ -0,0 +1,33 @@
+<?php
+require_once("php/header_login.php");
+?>
+
+        <div class="main_content">
+            <div class="login_formation">
+                <!-- Left ad -->
+                <section class="vertical_ad">
+                    advertentie
+                </section>
+
+                <!-- middle box -->
+                <form class="login_container" method="post" action="index.php">
+                    <h1 class="blue_top_text">Log in</h1>
+                    <label for="name">Gebruikersnaam</label>
+                    <input type="text" name="name">
+                    <label for="name">Wachtwoord</label>
+                    <input type="text" name="name">
+                    <input type="submit">
+                </form>
+
+                <!-- Right ad -->
+                <section class="vertical_ad">
+                    advertentie
+                </section>
+            </div>
+        </div>
+
+        <script src="https://kit.fontawesome.com/74f00c029b.js" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.5/gsap.min.js"></script>
+        <script src="js/animations.js"></script>
+    </body>
+</html>
