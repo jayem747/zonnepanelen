5ea7c6aa3a231b9537610377f2b375c81c62baff
RoanvDam <114396994+RoanvdamSchool@users.noreply.github.com>
2024-03-10 19:53:59 +0100

    product bewerken pagina af

--- edit_product.php
+++ edit_product.php
@@ -6,42 +6,57 @@ $sql = "SELECT * FROM producten";
 $products = $pdo->prepare($sql);
 $products->execute(array());
 ?>
-<div class="main_content two_sided_page">
-    <section class="page_left_side">
-        <form>
-            <label for="name">Naam</label>
-            <input type="text" name="name">
-            <table>
-                <tr>
-                    <th></th>
-                    <th></th>
-                </tr>
-                <tr>
-                    <td><label for="stock">Voorraad</label></td>
-                    <td><label for="price">Prijs</label></td>
-                </tr>
-                <tr>
-                    <td><input type="number" name="stock"></td>
-                    <td><input type="float" name="Prijs"></td>
-                </tr>
-            </table>
-            <label for="short_desc">Korte omschrijving</label>
-            <input type="text" name="short_desc">
-            <label for="whole_desc">volledige omschrijving</label>
-            <textarea name="whole_desc" rows="4" cols="50"></textarea>
-    </section>
-    <section class="page_right_side">
-        <p>Foto's</p>
-            <img src="img/solar_image.png">
-            <section class="input_next">
-                <input type="button" value="Bewerken"/>
-                <input type="button" value="verwijderen"/>
-            </section>
-            <input type="submit" value="Wijzigingen bevestigen">
-        </form>
-    </section>
+<div class="main_content">
+    <h1 class="blue_top_text">Bewerken product</h1>
+    <section class="divider_50px"><!-- extra space --></section>
+    <div class="two_sided_page">
+        <section class="page_left_side">
+            <form>
+                <label for="name">Naam</label>
+                <input type="text" name="name">
+                <section class="divider_25px"><!-- extra space --></section>
+                <table>
+                    <tr>
+                        <th></th>
+                        <th></th>
+                    </tr>
+                    <tr>
+                        <td><label for="stock">Voorraad</label></td>
+                        <td><label for="price">Prijs</label></td>
+                    </tr>
+                    <tr class="close_input">
+                        <td><input type="number" name="stock"></td>
+                        <td><input type="number" class="yellow_input"  step=0.01 name="Prijs"></td>
+                    </tr>
+                </table>
+                <section class="divider_25px"><!-- extra space --></section>
+                <label for="short_desc">Korte omschrijving</label>
+                <input type="text" name="short_desc">
+                <section class="divider_25px"><!-- extra space --></section>
+                <label for="whole_desc">volledige omschrijving</label>
+                <textarea name="whole_desc" rows="4" cols="50"></textarea>
+        </section>
+        <section class="page_right_side">
+            <p>Foto's</p>
+                <img src="img/solar_image.png" class="form_img_product">
+                <section class="divider_25px"><!-- extra space --></section>
+                <table class="double_buttons">
+                    <tr>
+                        <th></th>
+                        <th></th>
+                    </tr>
+                    <tr>
+                        <td><input type="button" class="blue_button_left" value="Toevoegen"/></td>
+                        <td><input type="button" class="yellow_button_right" value="verwijderen"/></td>
+                    </tr>
+                </table>
+                <section class="divider_25px"><!-- extra space --></section>
+                <input type="submit" value="Wijzigingen bevestigen" class="submit_dark_blue">
+            </form>
+        </section>
+    </div>
 </div>
-
+<section class="divider_150px"><!-- extra space --></section>
 <?php
 require_once("php/footer.php");
 ?>
