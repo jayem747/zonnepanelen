79eb6649796fc80ef52b37eb2bb4634fbd60c91a
Yorrick06 <yorrick06@casema.nl>
2024-03-20 08:31:27 +0100

    delete

--- delete_product.php
+++ delete_product.php
@@ -1,3 +1,23 @@
 <?php
+include("php/database_function.php");
+
+if (isset($_GET["productID"])) {
+    $productID = $_GET["productID"];
+    deleteProduct($productID);
+}
+
+function deleteProduct($productID) {
+    $conn = pdoObject("clearsky");
+
+    // Prepare SQL statement to delete the product
+    $sql = "DELETE FROM producten WHERE ProductID = :productID";
+    $stmt = $conn->prepare($sql);
+    $stmt->bindParam(':productID', $productID);
+    
+    // Execute the statement
+    $stmt->execute();
+
+    header("Location: home_admin.php");
+}
 
 ?>
