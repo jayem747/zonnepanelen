b522643d0a438278e963944a87924ba2b536fb88
Jayem <junemassa@gmail.com>
2024-04-03 12:22:41 +0200

    Update pakketten.php

--- pakketten.php
+++ pakketten.php
@@ -5,19 +5,20 @@ $pdo = pdoObject("clearsky");
 $sql = "SELECT * FROM producten";
 $products = $pdo->prepare($sql);
 $products->execute(array());
-
+$products = $products->fetchAll(PDO::FETCH_ASSOC);
 ?>
+
 <div class="main_content">
     <section class="divider_50px"><!-- divider --></section>
     <h1 class="blue_top_text">Pakketten</h1>
     <section class="divider_50px"><!-- divider --></section>
     <section id="products_grid">
         <?php foreach($products as $product) { 
-            $base64_image = base64_encode($product["Foto"])
+            $imagePath = $product["Foto"];
         ?>
         <section class="product_container">
             <section class="color_product_image">
-                <img src="data:image/jpeg;base64,<?=$base64_image?>" class="product_image">
+                <img src="<?=$imagePath?>" class="product_image">
             </section>
             <h2><?=$product["Titel"]?></h2>
             <p><?=$product["Omschrijving"]?></p>
